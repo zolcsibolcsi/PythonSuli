28655947338a85591394323975141e72e6423842
Holczer Zoltán <holczerz22@ADY-NAGYATAD.SULINET.HU>
2023-09-19 12:35:50 +0200

    Gyakorlas

---
+++ gyakorlas0919.py
@@ -0,0 +1,4 @@
+# írjuk ki a kétjegyű számokat
+
+for i in range (10,100):
+    print(i, end=" ")
---
+++ valtozok.py
@@ -0,0 +1,30 @@
+#változók próbálgatása
+
+
+szam=42
+
+print (szam)
+
+valosSzam=2.3
+
+print (valosSzam)
+
+szoveg="asdasdasdas"
+print (szoveg)
+
+sutANap=True
+print (sutANap)
+
+szam=szam+2
+print(szam)
+szam=szam+2
+print(szam)
+
+print(szam-10)
+print(szam/10)
+
+szam=12
+print(szam%12)
+
+szam=12
+print(szam//5)
