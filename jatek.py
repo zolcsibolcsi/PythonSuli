b7b7ab9fce7645ec4ea68b8b7d0bb2dc25bbc06d
Holczer Zoltán <holczerz22@ADY-NAGYATAD.SULINET.HU>
2023-11-15 08:15:38 +0100

    jatek

--- jatek.py
+++ jatek.py
@@ -16,42 +16,64 @@ lista=["előre", "hátra", "jobbra", "balra"]
 
 
 
-valasz=menu(lista)
+#valasz=menu(lista)
 #print(valasz,lista[valasz])
+#import szoveghun as t
+#Nyelv választás
+nyelvLista=["Magyar", "English", "Deutsch", "Russian"]
+nyelvId={"Magyar":"szoveghun","English":"szovegEng"}
+print("Válassz nyelvet:")
+while True:
+    nyelvValasztas=menu(nyelvLista)
+    print(nyelvLista[nyelvValasztas])
+    if nyelvLista[nyelvValasztas] in nyelvId:
+        break
+    else:
+        print("Sajnos ez a fordítás még nem készült el!")
 
+if nyelvId[nyelvLista[nyelvValasztas]] =="szoveghun":
+    import szoveghun as t
+elif nyelvId[nyelvLista[nyelvValasztas]] =="szovegEng":
+    import szovegEng as t
+else:
+    print(nyelvId[nyelvLista[nyelvValasztas]])
+
+
+    
 tortenet=[
         [
             1,#szál ID
-            "Reggel felébredtem. Mit tegyek?",#szöveg
-            ["fogmosás","reggeli","öltözés"],#választási lehetőségek
+            t.text["Reggel felébredtem. Mit tegyek?"],#szöveg
+            [t.text["fogmosás"],t.text["reggeli"],t.text["öltözés"]],#választási lehetőségek
             [2,3,4] #Hova ugorjon
         ],
         [
             2,#szál ID
-            "Elmegyek fogatmosni. Sikálom rendesen, ahogy kell!",#szöveg
-            ["fogmosás","reggeli","öltözés"],#választási lehetőségek
+            t.text["Elmegyek fogatmosni. Sikálom rendesen, ahogy kell!"],#szöveg
+            [t.text["fogmosás"],t.text["reggeli"],t.text["öltözés"]],#választási lehetőségek
             [2,3,4] #Hova ugorjon
         ],
         [
             3,#szál ID
-            "Kellene valamit enni! Anya csinált valamit? Nézzük meg!",#szöveg
-            ["fogmosás","reggeli","öltözés"],#választási lehetőségek
+            t.text["Kellene valamit enni! Anya csinált valamit? Nézzük meg!"],#szöveg
+            [t.text["fogmosás"],t.text["reggeli"],t.text["öltözés"]],#választási lehetőségek
             [2,3,4] #Hova ugorjon
         ],
         [
             4,#szál ID
-            "Kissé hűvös van, Kelle valami ruha! \nFelveszek egy nadrágot, meg egy pólót!",#szöveg
-            ["fogmosás","reggeli","öltözés"],#választási lehetőségek
+            t.text["Kissé hűvös van, Kelle valami ruha! \nFelveszek egy nadrágot, meg egy pólót!"],#szöveg
+            [t.text["fogmosás"],t.text["reggeli"],t.text["öltözés"]],#választási lehetőségek
             [2,3,4] #Hova ugorjon
         ],
         [
             66, #szál ID
-            "Vége mindennek..."#szoveg
+            "Vége mindennek...",#szoveg
             [],
             []
         ]
     ]
 
+        
 szalId=1
 
 while True:
---
+++ szoveghun.py
@@ -0,0 +1,9 @@
+text={  "Reggel felébredtem. Mit tegyek?":"Reggel felébredtem. Mit tegyek most?",
+        "fogmosás":"fogmosás",
+        "reggeli":"reggeli",
+        "öltözés":"öltözés",
+        "66-os parancs":"66-os parancs",
+        "Elmegyek fogatmosni. Sikálom rendesen, ahogy kell!":"Elmegyek fogatmosni. Sikálom rendesen, ahogy kell!",
+        "Kellene valamit enni! Anya csinált valamit? Nézzük meg!":"Kellene valamit enni! Anya csinált valamit? Nézzük meg!",
+        "Kissé hűvös van, Kelle valami ruha! \nFelveszek egy nadrágot, meg egy pólót!":"Kissé hűvös van, Kelle valami ruha! \nFelveszek egy nadrágot, meg egy pólót!"
+    }
